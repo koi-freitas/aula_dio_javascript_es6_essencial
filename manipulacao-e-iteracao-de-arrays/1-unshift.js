3cc751288cd00e90f9f131df2ff567240b3ff09c
koi-freitas <victorfrei555@gmail.com>
2021-01-29 18:41:45 -0300

    exemplo unshift

---
+++ manipulacao-e-iteracao-de-arrays/1-unshift.js
@@ -0,0 +1,8 @@
+const arr = ['banana', 'melancia', 'abacate'];
+const arrMaisLaranja = arr.unshift('laranja');
+
+console.log(arrMaisLaranja);
+// 4
+
+console.log(arr);
+// (4) ["laranja", "banana", "melancia", "abacate"]
