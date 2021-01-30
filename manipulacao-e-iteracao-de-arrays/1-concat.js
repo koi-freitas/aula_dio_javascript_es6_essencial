557c8bfa75ba38d498f2e934f98d6c51a698cfb1
koi-freitas <victorfrei555@gmail.com>
2021-01-30 09:59:33 -0300

    exemplo de concat

---
+++ manipulacao-e-iteracao-de-arrays/1-concat.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const novoArr = arr.concat(arr2);
+
+console.log(arr);
+// (3) [1, 2, 3]
+
+console.log(arr2);
+// (3) [4, 5, 6]
+
+console.log(novoArr);
+// (6) [1, 2, 3, 4, 5, 6]
