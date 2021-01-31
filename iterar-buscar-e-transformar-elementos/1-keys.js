62c65f9fe45b33cf89d3e2ceecbf8d34779b377c
koi-freitas <victorfrei555@gmail.com>
2021-01-31 11:05:03 -0300

    exemplo de keys

---
+++ iterar-buscar-e-transformar-elementos/1-keys.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, 3, 4];
+
+const arrIterator = arr.keys();
+
+arrIterator.next();
+// {value: 0, done: false}
+
+arrIterator.next();
+// {value: 1, done: false}
+
+arrIterator.next();
+// {value: 2, done: false}
+
+arrIterator.next();
+// {value: 3, done: false}
+
+arrIterator.next();
+// {value: undefined, done: true}
