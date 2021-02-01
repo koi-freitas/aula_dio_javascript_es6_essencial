a94b9c772f835811dacddd85cf67da6dde23f389
koi-freitas <victorfrei555@gmail.com>
2021-02-01 09:22:01 -0300

    correção do console.log

--- iterar-buscar-e-transformar-elementos/1-entries.js
+++ iterar-buscar-e-transformar-elementos/1-entries.js
@@ -3,16 +3,16 @@ const arr = [1, 2, 3, 4];
 const arrIterator = arr.entries();
 
 arrIterator.next();
-// {value: [0, 1], done: false}
+// {value: (2) [0, 1], done: false}
 
 arrIterator.next();
-// {value: [1, 2], done: false}
+// {value: (2) [1, 2], done: false}
 
 arrIterator.next();
-// {value: [2, 3], done: false}
+// {value: (2) [2, 3], done: false}
 
 arrIterator.next();
-// {value: [3, 4], done: false}
+// {value: (2) [3, 4], done: false}
 
 arrIterator.next();
 // {value: undefined, done: true}
