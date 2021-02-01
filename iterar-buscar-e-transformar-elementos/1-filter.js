f30ac89776c00d67831c11126f701a28e39443d2
koi-freitas <victorfrei555@gmail.com>
2021-02-01 09:45:36 -0300

    exemplo de filter

---
+++ iterar-buscar-e-transformar-elementos/1-filter.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 3, 4];
+
+const allValuesGreaterThanTwo = arr.filter(value => value > 2);
+
+console.log(allValuesGreaterThanTwo);
+// (2) [3, 4]
+console.log(arr);
+// (4) [1, 2, 3, 4]
